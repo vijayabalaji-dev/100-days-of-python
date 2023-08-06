c3bd67041e965dbcd4cfacb228870c24e0f15496
vijayabalaji-dev <89340609+vijayabalaji-dev@users.noreply.github.com>
2023-08-06 19:34:36 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("your name is "+input("what is your name ? "))
+#to add new line
+name = ("what is your name ? \n") 
+#escape character backslash
+print("name is \"vijay\"")
+#concatination operator +
