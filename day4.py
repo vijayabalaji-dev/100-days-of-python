90ed4c131e910930b41b83edbc49b735f540f750
vijayabalaji-dev <89340609+vijayabalaji-dev@users.noreply.github.com>
2023-08-08 08:27:04 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,15 @@
+#askpythom.com
+#to learn more about python modules
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+
+num = random.randint(0,1)
+
+if num == 0:
+    print("Tails")
+else:
+    print("Heads")
